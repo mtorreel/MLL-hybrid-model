7605dd6279b0fab911958ea1c0af74fca2a65f9c
mtorreel <Maxim.Torreele@UGent.be>
2022-05-08 14:03:50 +0200

    Include post-processing for paper results.

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ projectName = 'structure1_v1'
 nrOfSpaceSteps = 79 + 17 + 7 # number of spatial steps in active region
 folderName = 'D:/thesis_code/' + projectName # name of folder
 passiveLength = 37.4e-3 # length of the passive region (m)
-simulationTime = 100e-9 # total simulation time (s)
+simulationTime = 200e-9 # total simulation time (s)
 rightReflectivity = 0.99 # reflectivity of the right mirror
-current = 0.080 # current fed to the amplifier sections
+current = 0.070 # current fed to the amplifier sections
 withSSF = True # indicates whether simulation is performed using SSF (True) or propagation and delay (False)
 videoSamplingRate = -1 # rate of sampling for video (-1 means no video)
 pulseHeight = 0 # pulse height - 0 means default value
 isWithMemory = True # switch for photon and carrier memory
 # filter = [] # coefficient arrays for a custom filter
 filter = []
-is_fullOutput = False    # boolean to switch for recording at multiple locations in the laser structure
+is_fullOutput = True    # boolean to switch for recording at multiple locations in the laser structure
 
 print('Simulation started.')
 
-outputName = 'output_testOutput'
+outputName = 'output_70mA_withSSF'
 
 # Initialize the simulation
 sim = HybridSimulation(nrOfSpaceSteps, folderName, passiveLength, simulationTime, rightReflectivity, current=current, withSSF=withSSF, videoSamplingRate=videoSamplingRate, pulse_height=pulseHeight, isWithMemory=isWithMemory, filter=filter, is_fullOutput=is_fullOutput)
--- postprocessing.py
+++ postprocessing.py
@@ -5,27 +5,49 @@ from MLL_functions import getpulse, getspectrum
 plt.rc('font', size=15)
 
 # Load the data
-dataA = np.loadtxt('output/output_testOutput.txt', dtype=complex)
+
+def plotVerticalLine(x, height):
+    plt.plot(x * np.ones_like((1,2)), np.array([0.0, height]))
 
 timeStep = 1.035e-13
 lambda0 = 1.552e-6
 
-timeA = np.arange(len(dataA)) * timeStep
+colorA = ['blue', 'black', 'red']
 
-plt.plot(timeA, np.abs(dataA)**2)
-plt.xlabel('Time [s]')
-plt.ylabel('Power [W]')
-plt.show()
+f, ax = plt.subplots(1, 1)
+f1, ax1 = plt.subplots(1, 1)
 
-[valid, lambdas, spectrum, lambdas_envelope, envelope, lambdas_phases, phases] = getspectrum(timeStep, lambda0, dataA, get_envelope_and_phase=True)
-if valid == True:
-    freq_center = int(np.where(spectrum == max(spectrum))[0])
-    lambda_center = lambdas[freq_center]
-    plt.plot(lambdas_envelope, envelope)
-    # plt.xlim([lambda_center - 5, lambda_center + 5])
-    # plt.ylim([np.max(spectrum) - 20, 5 + np.max(spectrum)])
-    plt.xlabel('Wavelength [nm]')
-    plt.ylabel('Amplitude [10 dB per div]')
-    yticks = np.arange(np.min(envelope), np.max(envelope) + 5, step=10)
-    plt.yticks(yticks, labels=[' ' for i in range(len(yticks))])
+# Loop over different positions in the laser
+for i in range(3):
+    # Load the data
+    dataA = np.loadtxt('output/output_70mA_withSSF_full.txt', dtype=complex, usecols=i)
+
+    # Plot the pulse shape
+    [valid_flag, t_axis, trace_selection, trace_selection_fit, pulsewidth, pulseenergy] = getpulse(timeStep, dataA, 1e9)
+    maxNdx = np.argmax(np.abs(trace_selection)**2)
+    maxValue = np.max(np.abs(trace_selection)**2)
+    translated_t_axis = t_axis - t_axis[maxNdx]
+
+    ax.plot(translated_t_axis, np.abs(trace_selection)**2 / maxValue, label='Position ' + str(i + 1), c=colorA[i])
+
+    # Plot the spectrum envelope
+    [valid, lambdas, spectrum, lambdas_envelope, envelope, lambdas_phases, phases] = getspectrum(timeStep, lambda0, dataA, get_envelope_and_phase=True)
+    if valid == True:
+        print('PE = ' + str(pulseenergy))
+        print('PW = ' + str(pulsewidth))
+        freq_center = int(np.where(spectrum == max(spectrum))[0])
+        lambda_center = lambdas[freq_center]
+        ax1.plot(lambdas_envelope, envelope, label='Position ' + str(i + 1), c=colorA[i])
+
+ax.set_xlabel('$t - t_0$ [ps]')
+ax.set_ylabel('Amplitude [a.u.]')
+ax.legend()
+
+ax1.set_xlabel('Wavelength [nm]')
+ax1.set_ylabel('Amplitude [5 dB per div]')
+yticks = np.arange(np.min(envelope), np.max(envelope) + 5, step=5)
+ax1.set_yticks(yticks)
+ax1.set_yticklabels([' ' for i in range(len(yticks))])
+ax1.legend()
+
 plt.show()
